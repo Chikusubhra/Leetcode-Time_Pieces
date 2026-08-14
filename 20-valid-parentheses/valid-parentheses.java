433b9684b5ee7afbef9cc90a844bb732ee29c46c
Subhrajit Das <166319237+Chikusubhra@users.noreply.github.com>
2026-08-14 10:20:08 +0530

    Time: 2 ms (98.25%) | Memory: 43.3 MB (41.37%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,17 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        while(true){
-            if(s.contains("()")){
-                s=s.replace("()","");
-            }
-            else if(s.contains("[]")){
-                s=s.replace("[]","");
-            }
-            else if(s.contains("{}")){
-                s=s.replace("{}","");
-            }else{
-                return s.isEmpty();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } 
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
             }
         }
+
+        return stack.isEmpty();
     }
 }
